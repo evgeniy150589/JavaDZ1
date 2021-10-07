fa7356bf50b6e371dcc1ad898aba2d3cf30257dc
evgeniy150589 <epritula89@mail.ru>
2021-10-07 13:11:06 +0300

    DZ1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int clientsBalance = 2_000_000_000;
+        int sumTransfer = 500_000_000;
+        int totalSum = clientsBalance + sumTransfer;
+        System.out.println(totalSum);
+    }
+
+}
